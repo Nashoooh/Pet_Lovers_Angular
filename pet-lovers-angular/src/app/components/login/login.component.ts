6c413154babf5f67778e2280e66a1ec9e4657adb
Ignacio Andana De Lapyera <Ignacioad92@gmail.com>
2025-06-12 20:05:22 -0400

    [UPDATE] actualiza proyecto con home

--- pet-lovers-angular/src/app/components/login/login.component.ts
+++ pet-lovers-angular/src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  email: string = '';
+  password: string = '';
+  userType: string = '';
+  rememberMe: boolean = false;
+  loginError: string | null = null;
+  tiposUsuario = [
+    { valor: 'socio', texto: 'Socio' },
+    { valor: 'admin', texto: 'Administrador' }
+  ];
 }
